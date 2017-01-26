8ee5ad305b6bb291ccfb0b79a06496d3ede4ac2a
Michael Mavris <michaelmavris@gmail.com>
2017-01-26 23:20:12 +0200

    Fixed issue with MAC Address at iOS 10.2
    Removed unnecessary files
    Updated MacFinder lib

---
+++ MMLanScanDemo/MMLanScanDemo/MacFinder.h
@@ -0,0 +1,22 @@
+//
+//  MacFinder.h
+//  MacFinder
+//
+//  Created by Michael Mavris on 08/06/16.
+//  Copyright © 2016 Miksoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#if TARGET_IPHONE_SIMULATOR
+#include <net/route.h>
+#else
+#include "route.h"
+#endif
+
+#include "if_ether.h"
+#include <arpa/inet.h>
+
+@interface MacFinder : NSObject;
++(NSString*)ip2mac: (NSString*)strIP;
+@end
