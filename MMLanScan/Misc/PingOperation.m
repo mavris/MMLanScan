3302572eb1609700c7c5d75ac3861790708f81da
Jelle Alten <jelle@MacBook-Pro-van-Jelle.local>
2017-05-28 09:08:20 +0200

    Avoid name resolving when we are just trying to ping an ip address

--- MMLanScan/Misc/PingOperation.m
+++ MMLanScan/Misc/PingOperation.m
@@ -38,7 +38,7 @@ static const float PING_TIMEOUT = 1;
     if (self) {
         self.name = ip;
         self.ipStr= ip;
-        self.simplePing = [SimplePing simplePingWithHostName:ip];
+        self.simplePing = [SimplePing simplePingWithHostAddress:self.addrDataFromIp];
         self.simplePing.delegate = self;
         self.result = result;
         _isExecuting = NO;
@@ -47,6 +47,17 @@ static const float PING_TIMEOUT = 1;
     }
     
     return self;
+};
+
+- (NSData *)addrDataFromIp {
+    struct sockaddr_in remoteAddr;
+    remoteAddr.sin_len = sizeof(remoteAddr);
+    remoteAddr.sin_family = AF_INET;
+    remoteAddr.sin_port = htons(161);
+    const char *ipCStr = [self.ipStr UTF8String];
+    inet_pton(AF_INET, ipCStr, &remoteAddr.sin_addr);
+
+    return [NSData dataWithBytes:&remoteAddr length:remoteAddr.sin_len];
 };
 
 -(void)start {
